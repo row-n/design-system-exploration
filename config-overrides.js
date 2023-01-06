659f61cfea8110ef07dd93d5407eba11f9c63753
Rowan Parkinson <rowan.parkinson@gmail.com>
2023-01-06 13:42:18 +1300

    Using NPM >6, upgrading dependencies and changing react-app-rewired for craco

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
-
-module.exports = function override(config) {
-  const newConfig = config;
-  newConfig.plugins = [new VanillaExtractPlugin()].concat(newConfig.plugins);
-
-  return newConfig;
-};
---
+++ craco.config.js
@@ -0,0 +1,7 @@
+const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+
+module.exports = {
+  webpack: {
+    plugins: [new VanillaExtractPlugin()],
+  },
+};
