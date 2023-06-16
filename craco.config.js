45648b3a51beac4a70e4fdfa33687d7746adfe76
Rowan Parkinson <rowan.parkinson@gmail.com>
2023-06-16 10:53:53 +1000

    Updating packages

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,24 @@
 const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const path = require('path');
 
 module.exports = {
   webpack: {
-    plugins: [new VanillaExtractPlugin()],
+    configure: (webpackConfig) => {
+      // This `configure` setup is required to create a production build as CRA doesn't allow you to import files outside the src/ directory
+      // Reference: https://sandroroth.com/blog/vanilla-extract-cra
+
+      const moduleScopePlugin = webpackConfig.resolve.plugins.find(
+        (plugin) => plugin instanceof ModuleScopePlugin
+      );
+      moduleScopePlugin.allowedPaths.push(
+        path.resolve(__dirname, 'node_modules/@vanilla-extract/webpack-plugin')
+      );
+
+      return webpackConfig;
+    },
+    plugins: {
+      add: [new VanillaExtractPlugin()],
+    },
   },
 };
