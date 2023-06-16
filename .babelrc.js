05cdd93302ab29fe24ab84d6674d0cae3a0a1cd1
Rowan Parkinson <rowan.parkinson@gmail.com>
2023-06-16 11:46:50 +1000

    Replacing NPM with PNPM

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,6 @@
-module.exports = api => {
+module.exports = (api) => {
   api.cache(true);
   return {
-    presets: [
-      "@babel/preset-env",
-      "@babel/preset-react",
-    ],
-    plugins: [
-      "@vanilla-extract/babel-plugin",
-    ]
-  }
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+  };
 };
